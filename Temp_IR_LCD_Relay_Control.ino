29133c393653f7e8fe4b2478475545d4d2cb0a9e
Michael Nuro Spetås <michael-985@hotmail.com>
2013-06-20 04:19:35 +0300

    Added new feature: The set temperature for when the relay turns on is now adjustable via IR remote.

--- Temp_IR_LCD_Relay_Control.ino
+++ Temp_IR_LCD_Relay_Control.ino
@@ -11,47 +11,45 @@ Feel free to use and modify
 
 // Define the number of samples to keep track of. Higher number equals more smoothing
 // .. But it takes more time to display the average temperature.
-const int numReadings = 12;
+const int numReadings = 16;
 
 int readings[numReadings];      // the readings from the analog input
 int index = 0;                  // the index of the current reading
 int total = 0;                  // the running total
-int average = 0;                // the average
+int average = 0;                 // the average
 
-const int setTemp = 26;         // temperature when the relay turns on
-const int hysteresis = 3.5;      // hysteresis
+byte setTemp = 25;               // temperature when the relay turns on
+const byte hysterese = 3.5;      // hysterese
 
-const int RECV_PIN = 10;
-const int relayPin = 8;                
-const int rPin = 6;             // pin connected to the red LED
-const int gPin = 9;             // pin connected to the green LED
+const byte RECV_PIN = 10;
+const byte relayPin = 8;         // pin conntected to the relay    
+const byte rPin = 6;             // pin connected to the red LED
+const byte gPin = 9;             // pin connected to the green LED
+const byte inputPin = A0;
 
-float temp;
-float celcius;
-const int inputPin = A0;
+int tempAdj;                     // temperature adjusted after a voltage drop of 40mV
+int temp;
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
-boolean IRoverride = false;
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-#define lcdPin 7
+const byte lcdPin = 7;
 
 void setup()
 {
-  // initialize serial communication with computer:
-  Serial.begin(9600);
-  pinMode(rPin, OUTPUT);
+  Serial.begin(9600);            // serial communication with the computer for debugging
+  pinMode(rPin, OUTPUT);         // declaring the pins as outputs, default is input
   pinMode(gPin, OUTPUT);
   pinMode(relayPin, OUTPUT); 
   pinMode(lcdPin, OUTPUT);
   digitalWrite(lcdPin, OUTPUT);
   lcd.begin(16, 2);
-  lcd.print("Calibrating sensor:");
+  lcd.print("Calibrating sensor");
   digitalWrite(rPin, LOW);
   digitalWrite(gPin, HIGH);
   delay(500);
-  digitalWrite(gPin, LOW); // Blinking while waiting for the sensor to calibrate
+  digitalWrite(gPin, LOW);      // Blinking while waiting for the sensor to calibrate
   digitalWrite(rPin, HIGH);
   delay(500);
   digitalWrite(rPin, LOW);
@@ -59,12 +57,14 @@ void setup()
   delay(500);
   digitalWrite(gPin, HIGH);
   digitalWrite(rPin, HIGH);
-  lcd.begin(16, 2);
-  lcd.print("Temperature:");
-  lcd.setCursor(6, 1);
-  lcd.print("Standby");
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Standby:");
+  lcd.setCursor(0, 1);
+  lcd.print("smash the remote");
   
-  irrecv.enableIRIn(); // Start the receiver
+  
+  irrecv.enableIRIn();          // Start the receiver
   
   // initialize all the readings to 0: 
   for (int thisReading = 0; thisReading < numReadings; thisReading++)
@@ -72,55 +72,94 @@ void setup()
 }
 
 void loop() {  
- // subtract the last reading:
-  total= total - readings[index];         
-  // read from the sensor:  
-  readings[index] = analogRead(inputPin); 
-  // add the reading to the total:
-  total= total + readings[index];       
-  // advance to the next position in the array:  
-  index = index + 1;                    
-  // if we're at the end of the array...
-  if (index >= numReadings)            
-  //index &= (array_size-1);  
-    // ...wrap around to the beginning: 
-    index = 0;                           
-  // calculate the average:
-  average = total / numReadings;
-  temp = average * 0.48828125;
-  //temp = (average*5000)/1024; //Converts raw sensos value to voltage value
-  //celcius = temp/10;//sensor value*5000/bit^10 =1024, 10mV/1 degree celcius  
+  total= total - readings[index];           // subtract the last reading:    
+  readings[index] = analogRead(inputPin);   // read from the sensor:  
+  total= total + readings[index];           // add the reading to the total   
+  index = index + 1;                        // continue to the next reading  
+  if (index >= numReadings)                 // if we're at the end      
+    index = 0;                              // start over again                  
+  average = total / numReadings;            // calculating the average:
+  temp = (5.0 * average * 100.0) / 1024;
   Serial.println(temp);
   Serial.print("\t");
-  delay(25);        // delay in between reads for stability 
+  // temperature printing moved, due to a small voltage drop when the fan is
+  // if there was no voltage drop then all lcd printing should be done here
+  delay(50);        // delay in between reads for stability 
   if (irrecv.decode(&results)) {
+      if (results.value == 0xFF5AA5) // if #6 on remote is pressed
+      {
+        setTemp = setTemp + 1;     // increase setTemp by 1
+        lcd.clear();
+        lcd.setCursor(3, 1);
+        lcd.print("/");
+        lcd.setCursor(4, 1);
+        lcd.print(setTemp);
+        Serial.println("increasing setTemp");
+        if (setTemp > 35)          // if setTemp becomes larger than 35
+        {
+          setTemp = 35;             // then set it back to 35
+        }
+      }
+      if (results.value == 0xFF38C7) // #5 on remote
+      {
+        setTemp = setTemp - 1;      // decrease setTemp by 1
+        lcd.clear();
+        lcd.setCursor(3, 1);
+        lcd.print("/");
+        lcd.setCursor(4, 1);
+        lcd.print(setTemp);
+        Serial.println("decreasing setTemp");
+        if (setTemp < 20)          // if setTemp becomes smaller than 20
+        {
+          setTemp = 20;             // then set it back to 20
+        }
+      }
       if (results.value == 0xFF52AD || temp >= setTemp) // #9 on remote
       {
+        digitalWrite(relayPin, HIGH);    // fan is connected normally closed (NC)
         digitalWrite(rPin, HIGH);
         digitalWrite(gPin, LOW);
-        digitalWrite(relayPin, HIGH);
+        lcd.clear();
+        lcd.begin(16, 2);
+        lcd.print("Temperature:");
         lcd.setCursor(0, 1);
         lcd.print(temp);
-        lcd.setCursor(4, 1);
-        lcd.print((char)223); 
-        Serial.print("off");
+        lcd.setCursor(2, 1);
+        lcd.print((char)223);
         lcd.setCursor(6, 1);
-        lcd.println("       OFF");
+        lcd.print((char)223); 
+        lcd.setCursor(13, 1);
+        lcd.println("OFF");
+        lcd.setCursor(3, 1);
+        lcd.print("/");
+        lcd.setCursor(4, 1);
+        lcd.print(setTemp);
+        Serial.print("off");
         
       } 
-      else if (results.value == 0xFF4AB5 || temp - hysteresis <= setTemp ) // #8 on remote
+      if (results.value == 0xFF4AB5 || temp - hysterese <= setTemp ) // #8 on remote
       {
-        digitalWrite(relayPin, LOW);
+        digitalWrite(relayPin, LOW);      // fan is connected normally closed (NC)
         digitalWrite(rPin, LOW);
-        digitalWrite(gPin, HIGH);
+        digitalWrite(gPin, HIGH); 
+        lcd.clear();
+        lcd.begin(16, 2);
+        lcd.print("Temperature:");
         lcd.setCursor(0, 1);
-        lcd.print(temp + 4);  // temp adjusted after a 40 mili voltage drop when the fan turns on
-        lcd.setCursor(4, 1);
-        lcd.print((char)223); 
-        Serial.print("on");
+        tempAdj = temp + 4;
+        lcd.print(tempAdj);
+        lcd.setCursor(2, 1);
+        lcd.print((char)223);
         lcd.setCursor(6, 1);
-        lcd.println("        ON ");
+        lcd.print((char)223);
+        lcd.setCursor(13, 1);
+        lcd.println("ON ");
+        lcd.setCursor(3, 1);
+        lcd.print("/");
+        lcd.setCursor(4, 1);
+        lcd.print(setTemp);
+        Serial.print("on");
       }   
-    irrecv.resume(); // Receive the next IR signal
+    irrecv.resume(); // Receive the next value
  }
 }
